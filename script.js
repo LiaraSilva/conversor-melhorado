f416600c6c97f8721cf8b2339a11498feb3ad2a0
LiaraSilva <liara.nascimento@alunoescolassociais.org.br>
2024-11-01 10:04:06 -0300

    adiciona alteração

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = '5a0fb254ec789e44eadc9688';
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
-
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // Função para buscar taxa de câmbio via API
 async function getExchangeRate(daMoeda, paraMoeda){
     try{
@@ -12,7 +12,28 @@ async function getExchangeRate(daMoeda, paraMoeda){
         }else{
             throw new Error('Erro ao buscar as taxas de câmbio');
         }
-    }catch{
-
+    }catch(error){
+        console.error("Erro:", error);
+        return null;
     }
 } 
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+document.getElementById('currency-form').addEventListener('submit', async function(event){
+    event.preventDefault();
+    // obter valores de entrada
+    const valor = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+    // Busca taxa de câmbio da API
+    const exchangeRate = getExchangeRate(daMoeda, paraMoeda);
+
+    if(exchangeRate){
+        const converterValue = valor * exchangeRate;
+        
+        const conversao = document.getElementById('conversao');
+        conversao.textContent = `Resultado: ${converterValue.toFixed(2)} ${paraMoeda}`;
+    }else{
+        alert('Não foi possivel buscar a cotação. Tente novamente');
+    }
+
+})

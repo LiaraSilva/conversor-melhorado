7ec91a2c44f6b7d490980e4297d69ecdc796b27c
LiaraSilva <liara.nascimento@alunoescolassociais.org.br>
2024-11-01 07:45:57 -0300

    altera arquivo JS

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ async function getExchangeRate(daMoeda, paraMoeda){
     try{
         const response = await fetch(`${apiURL}${daMoeda}`);
         const data = await response.json();
+
+        if(data.result === "sucesso"){
+            return data.conversion_rate[paraMoeda];
+        }else{
+            throw new Error('Erro ao buscar as taxas de câmbio');
+        }
     }catch{
 
     }

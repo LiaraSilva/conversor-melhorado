c1d477662dc8fc0f2e7b70c9e248f870e810cc23
LiaraSilva <liara.nascimento@alunoescolassociais.org.br>
2024-11-01 07:36:07 -0300

    altera arquivi js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const apiKey = '5a0fb254ec789e44eadc9688';
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+// Função para buscar taxa de câmbio via API
+async function getExchangeRate(daMoeda, paraMoeda){
+    
+} 
